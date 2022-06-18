97f3564d3b2370e396a4a3592dd8eff994da1723
Rivan Albani Ray <rivanalbaniray88@gmail.com>
2022-06-18 10:05:05 +0700

    task9

---
+++ data/fruit.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ['mango', 'apple', 'Melon'];
+
+// TODO 2: export variable fruits
+module.exports = fruits;
